dcba297be98cab73c332c5605f64054eab9bbe14
Adel Fattakhova <fattakhovaadel@gmail.com>
2022-10-17 22:52:45 +0300

    implement queue based on linked list

---
+++ hw1/Queue.js
@@ -0,0 +1,42 @@
+import { DoublyLinkedList } from "./DoublyLinkedList.js";
+
+class Queue {
+  constructor() {
+    this.list = new DoublyLinkedList();
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(value) {
+    this.list.addEnd(value);
+    if (this.head === null) {
+      this.head = this.list.first.value;
+    }
+    this.tail = this.list.last.value;
+  }
+
+  pop() {
+    const removed = this.list.removeStart();
+    this.head = this.list.first?.value;
+    
+    if (removed.value) return removed.value;
+    throw(new Error(removed));
+  }
+}
+
+const queue = new Queue();
+
+queue.push(10);
+queue.push(11);
+queue.push(12);
+
+// console.log(queue.head);  // 10
+
+// console.log(queue.pop()); // 10
+
+// console.log(queue.head);  // 11
+// console.log(queue.tail);  // 12
+
+// console.log(queue.pop()); // 11
+// console.log(queue.pop()); // 12
+// console.log(queue.pop()); // Exception
